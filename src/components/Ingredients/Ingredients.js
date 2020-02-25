318c06fe6b078032b9443eebb51584fe775ceb5a
jadon <jadonwolffs@gmail.com>
2020-02-25 23:49:53 +0200

    intro to useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -6,6 +6,23 @@ import Search from "./Search";
 
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
+  useEffect(()=>{
+    fetch("https://react-hooks-http-requests.firebaseio.com/ingredients.json").then(response=>response.json()).then(
+    responseData=>{
+      const loaded =[];
+      for (const key in responseData) {
+          loaded.push({
+            id:key,
+            title:responseData[key].title,
+            amount:responseData[key].amount
+          })
+          setIngredients(loaded);
+          
+      }
+    }
+  )
+  },[]);
+  
   const removeIngredientHandler = id => {
     setIngredients(prevIngredients =>
       prevIngredients.filter(ingredient => ingredient.id !== id)
@@ -13,13 +30,24 @@ function Ingredients() {
     console.log(ingredients);
   };
   const addIngredientHandler = ingredient => {
-    setIngredients(prevIngredients => [
-      ...prevIngredients,
-      {
-        id: Math.random().toString(),
-        ...ingredient
-      }
-    ]);
+    fetch("https://react-hooks-http-requests.firebaseio.com/ingredients.json", {
+      method: "POST",
+      body: JSON.stringify(ingredient),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        setIngredients(prevIngredients => [
+          ...prevIngredients,
+          {
+            id: responseData.name,
+            ...ingredient
+          }
+        ]);
+      });
+
     console.log(ingredients);
   };
   return (
